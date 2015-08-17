b966972a6a6dc9cfe0720c4df87e088e7d3e03a4
fnalin <fnalin@msn.com>
2015-08-17 16:32:42 -0300

    Alterado binding style

--- Demos.KO/KO.Introducao/Models/Demos.cs
+++ Demos.KO/KO.Introducao/Models/Demos.cs
@@ -28,7 +28,8 @@ namespace KO.Introducao.Models
                     new Demo{ID=5,Nome="Binding - text",View="Binding_text",Descricao="Binding - text"},
                     new Demo{ID=6,Nome="Binding - html",View="Binding_html",Descricao="Binding - html"},
                     new Demo{ID=7,Nome="Binding - css",View="Binding_css",Descricao="Binding - css"},
-                    new Demo{ID=7,Nome="Binding - style",View="Binding_style",Descricao="Binding - style"},
+                    new Demo{ID=8,Nome="Binding - style",View="Binding_style",Descricao="Binding - style"},
+                    new Demo{ID=9,Nome="Binding - attr",View="Binding_attr",Descricao="Binding - style"},
                 };
             }
         }
